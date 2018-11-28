aa5ac0ccf74be994dc6b2aee0fabaaa8850dd61a
Aaron Clawson <clawsona@amazon.com>
2018-11-28 15:28:31 -0600

    This will create a deployment in an eks cluster

---
+++ pkg/apis/components/v1alpha1/deployment_types.go
@@ -0,0 +1,70 @@
+// Copyright 2018 Aaron Clawson
+// Copyright 2018 Alex Tanton
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+import (
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DeploymentSpec defines the desired state of Deployment
+type DeploymentSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	appsv1.DeploymentSpec `json:",inline"`
+	Cluster               string `json:"cluster"`
+}
+
+func (spec DeploymentSpec) GetDeploymentSpec() appsv1.DeploymentSpec {
+	return spec.DeploymentSpec
+}
+
+// DeploymentStatus defines the observed state of Deployment
+type DeploymentStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	appsv1.DeploymentStatus `json:",inline"`
+	Status                  string `json:"status,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Deployment is the Schema for the deployments API
+// +k8s:openapi-gen=true
+type Deployment struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DeploymentSpec   `json:"spec,omitempty"`
+	Status DeploymentStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// DeploymentList contains a list of Deployment
+type DeploymentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Deployment `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Deployment{}, &DeploymentList{})
+}
--- pkg/apis/components/v1alpha1/doc.go
+++ pkg/apis/components/v1alpha1/doc.go
@@ -12,3 +12,11 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Package v1alpha1 contains API Schema definitions for the components v1alpha1 API group
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=github.com/awslabs/aws-eks-cluster-controller/pkg/apis/components
+// +k8s:defaulter-gen=TypeMeta
+// +groupName=components.eks.amazonaws.com
+package v1alpha1
