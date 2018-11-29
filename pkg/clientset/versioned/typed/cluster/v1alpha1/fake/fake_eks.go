7846764c5f1ce061fa6f849ea202414919e1c62a
Tanton <tantonat@amazon.com>
2018-11-29 12:16:55 -0600

    fixed dep issue and locked codegen package version

--- pkg/clientset/versioned/typed/cluster/v1alpha1/fake/fake_eks.go
+++ pkg/clientset/versioned/typed/cluster/v1alpha1/fake/fake_eks.go
@@ -131,7 +131,7 @@ func (c *FakeEKSs) DeleteCollection(options *v1.DeleteOptions, listOptions v1.Li
 // Patch applies the patch and returns the patched eKS.
 func (c *FakeEKSs) Patch(name string, pt types.PatchType, data []byte, subresources ...string) (result *v1alpha1.EKS, err error) {
 	obj, err := c.Fake.
-		Invokes(testing.NewPatchSubresourceAction(ekssResource, c.ns, name, pt, data, subresources...), &v1alpha1.EKS{})
+		Invokes(testing.NewPatchSubresourceAction(ekssResource, c.ns, name, data, subresources...), &v1alpha1.EKS{})
 
 	if obj == nil {
 		return nil, err
