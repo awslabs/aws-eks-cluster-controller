fe8123bff447182c6228b09f4822f67b85c2e842
Anand <anand.modh@yahoo.com>
2018-12-05 15:24:38 -0600

    adding configmap components to k8s API (#15)
    * adding configmap components to k8s API

---
+++ pkg/apis/components/v1alpha1/configmap_types.go
@@ -0,0 +1,71 @@
+// Copyright 2018 Aaron Clawson
+// Copyright 2018 Alex Tanton
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ConfigMapSpec defines the desired state of ConfigMap
+type ConfigMapSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Cluster   string `json:"cluster"`
+	Name      string `json:"name"`
+	NameSpace string `json:"nameSpace"`
+
+	// +optional
+	Data map[string]string `json:"data,omitempty"`
+	// +optional
+	BinaryData map[string][]byte `json:"binaryData,omitempty"`
+}
+
+// ConfigMapStatus defines the observed state of ConfigMap
+type ConfigMapStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	Status string `json:"status,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ConfigMap is the Schema for the configmaps API
+// +k8s:openapi-gen=true
+type ConfigMap struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ConfigMapSpec   `json:"spec,omitempty"`
+	Status ConfigMapStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ConfigMapList contains a list of ConfigMap
+type ConfigMapList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ConfigMap `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ConfigMap{}, &ConfigMapList{})
+}
