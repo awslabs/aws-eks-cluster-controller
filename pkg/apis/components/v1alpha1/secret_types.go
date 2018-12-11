e10d3cb4f57a594d176f1764dc63b192cb746e40
Aaron Clawson <clawsona@amazon.com>
2018-12-11 11:19:37 -0600

    Created secret type

---
+++ pkg/apis/components/v1alpha1/secret_types.go
@@ -0,0 +1,75 @@
+// Copyright 2018 Aaron Clawson
+// Copyright 2018 Alex Tanton
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// SecretSpec defines the desired state of Secret
+type SecretSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+	// +optional
+	Data map[string][]byte `json:"data,omitempty"`
+	// +optional
+	StringData map[string]string `json:"stringData,omitempty"`
+	// +optional
+	Type corev1.SecretType `json:"type,omitempty"`
+
+	Cluster   string `json:"cluster"`
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+}
+
+// SecretStatus defines the observed state of Secret
+type SecretStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Status string `json:"status,omitempty"`
+}
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Secret is the Schema for the secrets API
+// +k8s:openapi-gen=true
+type Secret struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   SecretSpec   `json:"spec,omitempty"`
+	Status SecretStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// SecretList contains a list of Secret
+type SecretList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Secret `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Secret{}, &SecretList{})
+}
---
+++ pkg/controller/secret/secret_controller_test.go
@@ -0,0 +1,123 @@
+// Copyright 2018 Aaron Clawson
+// Copyright 2018 Alex Tanton
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package secret
+
+import (
+	"testing"
+	"time"
+
+	"github.com/awslabs/aws-eks-cluster-controller/pkg/authorizer"
+	"github.com/awslabs/aws-eks-cluster-controller/pkg/logging"
+
+	clusterv1alpha1 "github.com/awslabs/aws-eks-cluster-controller/pkg/apis/cluster/v1alpha1"
+	componentsv1alpha1 "github.com/awslabs/aws-eks-cluster-controller/pkg/apis/components/v1alpha1"
+	"github.com/onsi/gomega"
+	"golang.org/x/net/context"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+var c client.Client
+
+var expectedRequest = reconcile.Request{NamespacedName: types.NamespacedName{Name: "foo-secret", Namespace: "default"}}
+var secretKey = types.NamespacedName{Name: "foo-secret", Namespace: "default"}
+var rSecretKey = types.NamespacedName{Name: "remote-foo-secret", Namespace: "default"}
+
+const timeout = time.Second * 5
+
+func newTestReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileSecret{
+		Client: mgr.GetClient(),
+		scheme: mgr.GetScheme(),
+		log:    logging.New(),
+		auth:   authorizer.NewFake(mgr.GetClient()),
+	}
+}
+
+func TestReconcile(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	cluster := &clusterv1alpha1.EKS{
+		ObjectMeta: metav1.ObjectMeta{Name: "foo-eks", Namespace: "default"},
+		Spec: clusterv1alpha1.EKSSpec{
+			AccountID:            "1234",
+			ControlPlane:         clusterv1alpha1.ControlPlaneSpec{},
+			CrossAccountRoleName: "foo-role",
+			NodeGroups:           []clusterv1alpha1.NodeGroupSpec{},
+			Region:               "us-test-1",
+		},
+	}
+
+	instance := &componentsv1alpha1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: "foo-secret", Namespace: "default"},
+		Spec: componentsv1alpha1.SecretSpec{
+			Name:       "remote-foo-secret",
+			Namespace:  "default",
+			Cluster:    "foo-eks",
+			StringData: map[string]string{"key": "value"},
+		},
+	}
+
+	// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
+	// channel when it is finished.
+	mgr, err := manager.New(cfg, manager.Options{})
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	c = mgr.GetClient()
+
+	recFn, requests := SetupTestReconcile(newTestReconciler(mgr))
+	g.Expect(add(mgr, recFn)).NotTo(gomega.HaveOccurred())
+
+	stopMgr, mgrStopped := StartTestManager(mgr, g)
+
+	defer func() {
+		close(stopMgr)
+		mgrStopped.Wait()
+	}()
+
+	g.Expect(c.Create(context.TODO(), cluster)).NotTo(gomega.HaveOccurred())
+	defer c.Delete(context.TODO(), cluster)
+
+	// Create the Secret object and expect the Reconcile and Deployment to be created
+	err = c.Create(context.TODO(), instance)
+	// The instance object may not be a valid object because it might be missing some required fields.
+	// Please modify the instance object by adding required fields and then remove the following if statement.
+	if apierrors.IsInvalid(err) {
+		t.Logf("failed to create object, got an invalid object error: %v", err)
+		t.Fail()
+		return
+	}
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
+
+	rSecret := &corev1.Secret{}
+	err = c.Get(context.TODO(), rSecretKey, rSecret)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+
+	secret := &componentsv1alpha1.Secret{}
+	g.Eventually(func() (string, error) {
+		err := c.Get(context.TODO(), secretKey, secret)
+		return secret.Status.Status, err
+	}, timeout).Should(gomega.Equal("Created"))
+	g.Expect(c.Delete(context.TODO(), instance)).Should(gomega.Succeed())
+	g.Eventually(requests, timeout).Should(gomega.Receive(gomega.Equal(expectedRequest)))
+	g.Eventually(func() error { return c.Get(context.TODO(), rSecretKey, rSecret) }).Should(gomega.HaveOccurred())
+}
