cf64d84d0cbedb55ddd26a6269f7cdec8ddc55d3
Aaron <clawsona@amazon.com>
2019-04-05 11:56:46 -0500

    updated versions, regions, and amis
    Added v1.12
    removed v1.10
    updated amis
    added regions eu-west-2 ,eu-west-3, ap-northeast-2, and ap-south-1

--- pkg/apis/cluster/v1alpha1/regions.go
+++ pkg/apis/cluster/v1alpha1/regions.go
@@ -8,7 +8,11 @@ var SupportedRegions = []string{
 	"eu-central-1",
 	"eu-north-1",
 	"eu-west-1",
+	"eu-west-2",
+	"eu-west-3",
 	"ap-northeast-1",
+	"ap-northeast-2",
+	"ap-south-1",
 	"ap-southeast-1",
 	"ap-southeast-2",
 }
