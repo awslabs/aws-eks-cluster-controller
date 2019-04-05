65958e3d236468995ebded76e12b0df1a6a6a645
Aaron Clawson <Aaron.Clawson@gmail.com>
2019-04-05 14:19:59 -0500

    updated versions, regions, and amis (#73)
    Added v1.12
    removed v1.10
    updated amis
    added regions eu-west-2 ,eu-west-3, ap-northeast-2, and ap-south-1

--- pkg/apis/cluster/v1alpha1/versions.go
+++ pkg/apis/cluster/v1alpha1/versions.go
@@ -2,12 +2,12 @@ package v1alpha1
 
 // SupportedVersions is the list of current supported verisons of EKS (AC 2019-01-15) (https://docs.aws.amazon.com/eks/latest/userguide/platform-versions.html)
 var SupportedVersions = []string{
-	"1.10",
 	"1.11",
+	"1.12",
 }
 
 // DefaultVersion is the version that is used if none is supplied
-const DefaultVersion string = "1.11"
+const DefaultVersion string = "1.12"
 
 func isSupportedVersion(version *string) bool {
 	if version == nil {
